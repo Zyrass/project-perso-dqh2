9965dbf4dd2b37bf442293a36eedbc68ef9efe37
Alain Guillon <zyrass@outlook.fr>
2017-06-01 22:45:50 +0200

    Quelques modif sur la page ingredient

--- App/Zyrass/Models/ingredients.php
+++ App/Zyrass/Models/ingredients.php
@@ -3,19 +3,30 @@
     // Connexion à la bdd
     include '../Controllers/Connexion/bdd_connexion.php';
 
-    $req = $dsn->prepare('SELECT id, name, img FROM ingredients ORDER BY name');
-    $req->execute();
-    $info_ingredients = $req->fetchAll();
 
     $req = $dsn->prepare(
-        'SELECT monsters.name AS "name_monster", monsters.image_monster AS "image_monster"
+        'SELECT id, name, img 
         FROM ingredients 
-        INNER JOIN monsters_ingredients ON monsters_ingredients.ingredients_id = ingredients.id
-        INNER JOIN monsters ON monsters_ingredients.monsters_id = monsters.id
-        WHERE ingredients.id = 8'
-    );
+        ORDER BY name
+    ');
     $req->execute();
-    $info_loot = $req->fetchAll();
+    $info_all_loot = $req->fetchAll();
+
+
+    $req = $dsn->prepare(
+        'SELECT
+            ingredients.id      AS "id_ingredient",
+            ingredients.name    AS "name_ingredient",
+            ingredients.img     AS "image_ingredient",
+            monsters.name       AS "name_monster"
+        FROM monsters_ingredients 
+        INNER JOIN monsters ON monsters.id = monsters_ingredients.monsters_id
+        INNER JOIN ingredients ON ingredients.id = monsters_ingredients.ingredients_id
+        WHERE ingredients.id = ?'
+    );
+    $req->execute(array("id"));
+    $info_id_loot = $req->fetch();
+
 
     // Sélection et affichage du template "phtml"
     $template = 'ingredients';
